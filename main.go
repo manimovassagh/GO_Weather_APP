5a0ad8358b562362309dc9c1b6f0354d2e356129
Mani Movassagh <mani.mgh@gmail.com>
2022-07-03 12:35:48 +0200

    more

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -33,26 +32,25 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 	return c, nil
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello From Go Application!\n"))
+}
 
 func main() {
 	http.HandleFunc("/hello", hello)
 
-	http.HandleFunc("/weather", 
-func(w http.ResponseWriter, r *http.Request) {
-	 city:=strings.SplitN(e.URL.Path,"/",3)[2]
-	 data,err:=query(city)
-	 if err!=nil{
-		 http.Error(w,err.Error(),http.StatusInternalServerError)
-		 return
-	 }
-	 w.Header().Set("Content-Type","application/json")
-}
-)
+	http.HandleFunc("/weather",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(data)
+		})
 
-	http.ListenAndServe(":8080",nil)
-
-
+	http.ListenAndServe(":8080", nil)
 
 }
-
-
