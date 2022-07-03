5b98f111b823bda8c6423802bd9e4ecfa9c3ac52
Mani Movassagh <mani.mgh@gmail.com>
2022-07-03 12:27:09 +0200

    add http

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type apiConfigData struct {
+	openWeatherMapApiKey string `json:"openWeatherMapApiKey`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadApiConfig(filename string) (apiConfigData, error) {
+
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	var c apiConfigData
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	return c, nil
+}
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather", hello)
+
+	http.ListenAndServe(":8080",nil)
+
+
+
 }
+
+
